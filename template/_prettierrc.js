e2dcccbb6840de5a8f6721b537e141f358cbe292
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-12 17:13:30 +0200

    chore: update prettier configurations

--- template/_prettierrc.js
+++ template/_prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
 };
