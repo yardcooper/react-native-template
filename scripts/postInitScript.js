fdf637e98b1db9d16f17c16619421c6b02dc53ce
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-20 13:28:58 +0200

    chore: adjust postinitscript

--- scripts/postInitScript.js
+++ scripts/postInitScript.js
@@ -1,19 +1,3 @@
 #!/usr/bin/env node
 
-const ora = require('ora');
-const { execSync } = require("child_process");
-
-const spinner = ora('Fastlane setup');
-
 console.log('Commencing post-init script')
-
-new Promise((resolve) => {
-  spinner.start();
-  execSync('cd ios && bundle install');
-  resolve();
-}).then(() => {
-  spinner.succeed();
-}).catch((error) => {
-  spinner.fail(error);
-  throw new Error('Something went wrong during the post init script execution');
-});
