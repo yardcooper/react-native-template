45da5b80b88233af367b2087d206965346a4b467
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-22 15:30:27 +0200

    chore: adjust script location

--- template.config.js
+++ template.config.js
@@ -2,5 +2,5 @@ module.exports = {
   placeholderName: 'MyProject',
   templateDir: './template',
   titlePlaceholder: 'Template App',
-  postInitScript: './scripts/script.js',
+  postInitScript: './postInitScript.js'
 };
