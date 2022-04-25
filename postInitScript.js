2eb95f08ce51d175ecb25f4dc7f8ada27704bda5
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-25 09:58:38 +0200

    Revert "chore:adjust script"
    This reverts commit 0e5b1cea75ce95df0c80cfce4da83292e9328e8f.

--- postInitScript.js
+++ postInitScript.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
 
-const { execSync, spawnSync } = require("child_process");
-
-// initialise empty git repo for Husky to work
-const projectPath = process.cwd();
-execSync(`git init "${projectPath}"`);
+console.log("Script run");
