a74534bde3a6f73859459db63ca29970e199f2a8
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-25 14:18:37 +0200

    chore: adjust script

--- postInitScript.js
+++ postInitScript.js
@@ -1,23 +1,3 @@
 #!/usr/bin/env node
 
-// To handle CLI commands)
-// const { execSync } = require("child_process";
-const ora = require("ora");
-
-const spinner = ora("Executing post init script ");
-
-new Promise((resolve) => {
-  spinner.start();
-  const projectPath = process.cwd();
-  console.log(`Project created at ${projectPath}`);
-  resolve();
-})
-  .then(() => {
-    spinner.succeed();
-  })
-  .catch(() => {
-    spinner.fail();
-    throw new Error(
-      "Something went wrong during the post init script execution"
-    );
-  });
+console.log(`\nPost init script complete!`)
