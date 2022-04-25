fc75be039ac4c3f1c3aa388643299dcd0e72a290
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-25 11:51:31 +0200

    chore: update script

--- postInitScript.js
+++ postInitScript.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const { execSync, spawnSync } = require("child_process");
+const { execSync } = require("child_process");
 
 // Install Fastlane iOS
 const projectPath = process.cwd();
-execSync(`cd "${projectPath}/ios bundle install"`);
+execSync(`cd "${projectPath}/ios && bundle install"`);
 console.log('Script complete')
