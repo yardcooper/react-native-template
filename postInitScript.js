dd1fb7ce850ac89e7a0aab2f5080e913f237de8e
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-25 08:52:18 +0200

    chore:adjust script

--- postInitScript.js
+++ postInitScript.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 
-console.log("Script run");
+const { execSync, spawnSync } = require("child_process");
+
+// initialise empty git repo for Husky to work
+const projectPath = process.cwd();
+execSync(`git init "${projectPath}"`);
