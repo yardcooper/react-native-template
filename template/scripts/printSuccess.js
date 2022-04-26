ad243066c9638e46a720f72fecb5942d00915cff
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-26 11:43:49 +0200

    docs: add comment

--- template/scripts/printSuccess.js
+++ template/scripts/printSuccess.js
@@ -1,5 +1,7 @@
 const chalk = require('chalk');
 
+// TODO: Update with 'ora' usage when CLI package is updated.
+// See: https://github.com/react-native-community/cli/issues/1492#issuecomment-1109474814
 module.exports = {
   async success() {
     return new Promise((resolve) => {
