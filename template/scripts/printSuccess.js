c179f67ae79a0064dfba1ac10b18da61f269b613
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-26 09:48:03 +0200

    chore: adjust script

--- template/scripts/printSuccess.js
+++ template/scripts/printSuccess.js
@@ -1,18 +1,10 @@
-const ora = require("ora");
-
-const spinner = ora("Executing post init script ");
-
 module.exports = {
   async success() {
     return new Promise((resolve) => {
       console.log("\nTemplate initialization successful! 🚀");
       resolve();
     })
-      .then(() => {
-        spinner.succeed();
-      })
       .catch(() => {
-        spinner.fail();
         throw new Error(
           "Something went wrong during the post init script execution"
         );
