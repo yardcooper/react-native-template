eaefb74616779ffeb3dbc282c2fc50ce37392289
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-04-26 11:32:05 +0200

    chore: adjust script printing

--- template/scripts/printSuccess.js
+++ template/scripts/printSuccess.js
@@ -3,13 +3,15 @@ const chalk = require('chalk');
 module.exports = {
   async success() {
     return new Promise((resolve) => {
-      console.log(`\n${chalk.green('✔')} Template initialization successful! 🚀`);
+      console.log(
+        '\033[2A',
+        `${chalk.green('✔')} Template initialization successful! 🚀`
+      );
       resolve();
-    })
-      .catch(() => {
-        throw new Error(
-          "Something went wrong during the post init script execution"
-        );
-      });
+    }).catch(() => {
+      throw new Error(
+        "Something went wrong during the post init script execution"
+      );
+    });
   },
 };
