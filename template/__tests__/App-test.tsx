31159b79ea90f71db160df2bbb9d04999a0e9503
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-11-03 17:10:47 +0100

    chore: reduce number of async edge-cases in jest

--- template/__tests__/App-test.tsx
+++ template/__tests__/App-test.tsx
@@ -1,12 +1,15 @@
 import 'react-native';
 import React from 'react';
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { act, create } from 'react-test-renderer';
 
 import App from '../src/App';
 
 jest.useFakeTimers();
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders correctly', async () => {
+  const render = create(<App />);
+  await act(async () => {
+    expect(render).toBeDefined();
+  });
 });
