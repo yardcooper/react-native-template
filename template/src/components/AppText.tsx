5e1870be5e62ed976645623bc4afa4ffc33ddee7
yardcooper <colincrick.lovestobecreative@outlook.com>
2022-11-16 08:49:49 +0100

    chore: add apptext component

---
+++ template/src/components/AppText.tsx
@@ -0,0 +1,46 @@
+import React, { FunctionComponent } from 'react';
+import { StyleSheet, Text, TextProps } from 'react-native';
+
+type AppTextType = 'BodyLarge' | 'BodyMedium' | 'BodySmall';
+
+interface Props extends TextProps {
+  type?: AppTextType;
+}
+
+const AppText: FunctionComponent<Props> = ({
+  type,
+  style,
+  children,
+  ...props
+}) => {
+  const textStyle = type ? textStyles[type] : textStyles.BodyMedium;
+
+  return (
+    <Text style={[textStyle, style]} {...props}>
+      {children}
+    </Text>
+  );
+};
+
+export default AppText;
+
+export const textStyles = StyleSheet.create({
+  BodyLarge: {
+    fontSize: 19,
+    lineHeight: 28,
+    letterSpacing: 0.38, // 2%
+    color: 'black',
+  },
+  BodyMedium: {
+    fontSize: 17,
+    lineHeight: 24,
+    letterSpacing: 0.34, // 2%
+    color: 'black',
+  },
+  BodySmall: {
+    fontSize: 14,
+    lineHeight: 20,
+    letterSpacing: 0.28, // 2%
+    color: 'black',
+  },
+});
